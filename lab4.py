c77997558628db1e8c25eb90eae2f5e3c411ec03
Нестеренко Ирина <nstrnkrn@yandex.com>
2024-11-05 00:21:17 +0700

    POST/Redirect/GET

--- lab4.py
+++ lab4.py
@@ -156,4 +156,9 @@ def tree() :
     elif operation == 'plant':
         tree_count += 1
 
-    return render_template('lab4/tree.html', tree_count=tree_count)    
+    return redirect('/lab4/tree') 
+
+    return render_template('lab4/tree.html', tree_count=tree_count) 
+
+ 
+ 
